2013b07900284afe200cb0ae4933e9912ef406fd
Marco Afonso Albino Ramião <marcoafonso1706@gmail.com>
2026-09-08 00:16:02 +0100

    manually added a command line argument parser to add support to choose whether to color the ascii image or not

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,7 @@ type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 pub fn convert_to_ascii(image_path: &str) -> Result<impl Iterator<Item = char>> {
   let image = 
-  ImageReader::open(image_path)
-    .unwrap()
+  ImageReader::open(image_path)?
     .decode()?;
 
   let ds_image = resize(&image);
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,54 @@
-use clap::Parser;
 use std::error::Error;
 
 use image_to_ascii::convert_to_ascii;
 
+const COLOR_FLAG: &str = "-color";
+const NOT_ENOUGH_ARGS_ERR: &str = 
+r#"Not enough arguments
+
+Usage:
+cargo run -- <IMAGE_PATH> [COLOR_ASCII]"#;
+
 fn main() {
-    let config = Config::parse();
+    let config = Config::build(std::env::args())
+        .unwrap_or_else(|e| {
+            eprintln!("Error: {e}");
+            std::process::exit(1)
+    });
 
     if let Err(e) = run(config) {
-        println!("Error: {e}");
+        eprintln!("Error: {e}");
     }
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    for c in convert_to_ascii(&config.path)? {
-        print!("{c}");
-    }
+   println!("Configuration: {config:?}");
+
+    // for c in ascii {
+    //     print!("{c}");
+    // }
     Ok(())
 }
 
-// fn change_file_name(file_name: &str) -> String {
-//     file_name.replacen(".", "_ds.", 1)
-// }
-
-#[derive(Parser)]
+#[derive(Debug)]
 struct Config {
-    path: String,
+    path:        String,
+    color_ascii: bool
+}
+
+impl Config {
+    fn build(
+        mut args: std::env::Args
+    ) -> Result<Config, &'static str> {
+        args.next();
+        let Some(path) = args.next() else {
+            return Err(NOT_ENOUGH_ARGS_ERR);
+        };
+        let color_ascii = match args.next() {
+            Some(flag) => flag == COLOR_FLAG,
+            None => false,
+        };
+
+        Ok( Config { path, color_ascii } )
+    }
 }
